647c8f99ff447818f0f6c4ecff8d28a42a2cb573
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-12-02 16:45:46 +0200

    Add Youtube redirect

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::{header, StatusCode},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
 async fn hello_bird() -> &'static str {
     "Hello, bird!"
 }
 
+async fn see_bird() -> Response {
+    (
+        StatusCode::FOUND,
+        [(
+            header::LOCATION,
+            "https://www.youtube.com/watch?v=9Gc4QTqslN4",
+        )],
+    )
+        .into_response()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_bird));
+    let router = Router::new()
+        .route("/", get(hello_bird))
+        .route("/-1/seek", get(see_bird));
 
     Ok(router.into())
 }
